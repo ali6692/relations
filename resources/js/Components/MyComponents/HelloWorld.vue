33e2c7631c74e4e918c5386ca3418a80098b980d
Faraz <farazmsi2014@gmail.com>
2023-07-06 17:54:33 +0530

    click event

--- resources/js/Components/MyComponents/HelloWorld.vue
+++ resources/js/Components/MyComponents/HelloWorld.vue
@@ -1,27 +1,26 @@
 <template>
-<h1>Email: {{email}}</h1>
-<h1>Name: {{getName("Inshu")}}</h1>
+<h1 v-on:mousemove="getConsole()">Home Component</h1>
+<button v-on:click="getData('Button 1 clicked')">Click me</button>
+<button v-on:dblclick="getData('Button 2 clicked')">Click Me</button>
 </template>
+
 <script>
 export default {
-    name: 'HelloWorld',
-    data()
-    {
-        return {
-            email:"abc@gmail.com",
-        }
-    },
-    methods: {
-        getName(name)
+    name:"HelloWorld",
+    methods:{
+        getData(data)
         {
-            return name;
+            this.count=10;
+        },
+        getConsole()
+        {
+            console.warn("function called")
         }
     }
 }
 </script>
 <style scoped>
-h1{
-    color:magenta;
+h1 {
+    color:darkcyan;
 }
 </style>
-
