c56dc34def5c6754206d6c0e603d6b7d0ca054ef
Faraz <farazmsi2014@gmail.com>
2023-07-08 15:13:59 +0530

    for loop

--- resources/js/Components/MyComponents/HelloWorld.vue
+++ resources/js/Components/MyComponents/HelloWorld.vue
@@ -1,23 +1,51 @@
 <template>
-
-<h1 v-if="show">If condition</h1>
-<h1 v-else>Else condition</h1>
-
-<button v-on:click="show=!show">Toggle</button>
+<h1>For loop in Vue Js</h1>
+<ul>
+    <li v-for="item in technology" :key="item">
+        {{item}}
+    </li>
+</ul>
+<br />
+<br />
+<ul>
+    <li v-for="item in user" :key="item.email">
+    Name is : {{item.name}} and Email is : {{item.email}}
+    </li>
+</ul>
 </template>
-
 <script>
 export default {
-    name:"HelloWorld",
-    data()
-    {
+    name: "HelloWorld",
+    data() {
         return {
-            show: true
+            technology: ['java','php','node','html'],
+            user: [{
+                name: "Faraz",
+                email: "abc@gmail.com"
+            },
+                {
+                    name:"Insha",
+                    email:"def@gmail.com"
+                },
+                {
+                    name:"Sana",
+                    email:"ghi@gmail.com"
+                },
+                {
+                    name:"Iqra",
+                    email:"jkl@gmail.com"
+                },
+                ]
         }
     }
 }
-
 </script>
+
+
+
+
+
+
 <style scoped>
 h1 {
     color:darkcyan;
