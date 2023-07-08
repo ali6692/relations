b73377ca45d47a664cad1c6f03caf0ca49553c1b
Faraz <farazmsi2014@gmail.com>
2023-07-08 14:21:05 +0530

    if condition and else condition

--- resources/js/Components/MyComponents/HelloWorld.vue
+++ resources/js/Components/MyComponents/HelloWorld.vue
@@ -1,28 +1,9 @@
 <template>
-<h1>Get checkbox & radio button value</h1>
-<h3>Technology</h3>
 
-<label for ="java">Java</label>
-<input type="checkbox" value="java" v-model="technology" id="java">
+<h1 v-if="show">If condition</h1>
+<h1 v-else>Else condition</h1>
 
-<label for="php">Php</label>
-<input type="checkbox" value="php" v-model="technology" id="php">
-
-<label for="Node">Node</label>
-<input type="checkbox" value="node" v-model="technology" id="node">
-
-<br /><br />
-<h3>Who am I</h3>
-<label for="student">Student</label>
-<input type="radio" value="student" v-model="who" name="who" id="student">
-
-<label for="developer">Developer</label>
-<input type="radio" value="php" v-model="who" name="who" id="developer">
-
-<br /><br />
-<h4>Selected Technology: {{technology}}</h4>
-<h4>I am a: {{who}}</h4>
-
+<button v-on:click="show=!show">Toggle</button>
 </template>
 
 <script>
@@ -31,8 +12,7 @@ export default {
     data()
     {
         return {
-            technology:[],
-            who:null
+            show: true
         }
     }
 }
