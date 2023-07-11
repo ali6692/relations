36e8c1c6e2fce6799dd9d8c60189e182463aaa24
Faraz <farazmsi2014@gmail.com>
2023-07-11 12:54:30 +0530

    class binding

--- resources/js/Components/MyComponents/HelloWorld.vue
+++ resources/js/Components/MyComponents/HelloWorld.vue
@@ -1,24 +1,44 @@
 <template>
-    <h1>{{text}}</h1>
-    <div v-html="tag"></div>
-    <div v-html="tag2"></div>
+    <h1>CSS binding</h1>
+    <h2 :class="applyStyle">Home Component</h2>
+    <button v-on:click="colorfull=!colorfull">Apply style</button>
+
 
 </template>
 <script>
 export default {
-    name:"HelloWorld",
-    data()
-    {
+    name: "HelloWorld",
+    data() {
         return {
-            text:"Faraz ALi Khan",
-            tag:"<h1>Faraz Ali Khan</h1>",
-            tag2:"<h1>abc@gmail.com</h1>"
+            colorfull: true
         }
-    }
+    },
+    computed:
+        {
+            applyStyle() {
+                return {
+                    skyblue: this.colorfull,
+                    err: true,
+                    other: true
+                }
+            }
+        }
 }
 </script>
 <style scoped>
 h1 {
-    color:darkcyan;
+    color:brown;
+}
+.skyblue{
+    background-color: skyblue;
+    width:250px;
+    padding:20px;
+}
+.err{
+    color:red;
+}
+.other{
+    font-size: 50px;
 }
 </style>
+
