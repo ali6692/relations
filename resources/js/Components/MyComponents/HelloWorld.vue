244944c0f2f8164ccd77b3b54cc17029a09eb8ea
Faraz <farazmsi2014@gmail.com>
2023-07-12 17:14:22 +0530

    complete form

--- resources/js/Components/MyComponents/HelloWorld.vue
+++ resources/js/Components/MyComponents/HelloWorld.vue
@@ -1,12 +1,37 @@
 <template>
     <h1>Simple form</h1>
     <form>
+
         <label>Email :</label>
         <input type="text" placeholder="Enter your email" v-model="form.email"/>
         <br/><br/>
 
         <label>Password :</label>
         <input type="password" placeholder="Enter your password" v-model="form.password"/>
+        <br/><br/>
+        <select v-model="form.country">
+            <option>India</option>
+            <option>USA</option>
+            <option>UK</option>
+        </select>
+
+        <h3>Technology</h3>
+        <label>Java :</label>
+        <input type="checkbox" value="java"   v-model="form.technology"/>
+        <br/><br/>
+        <label>.Net :</label>
+        <input type="checkbox" value=".net" v-model="form.technology"/>
+        <br/><br/>
+        <label>PHP :</label>
+        <input type="checkbox" value="php"  v-model="form.technology"/>
+        <br/><br/>
+
+        <h3>Gender</h3>
+        <label>Male</label>
+        <input type="radio" value="male" name="gender" v-model="form.gender"/>
+        <br/><br/>
+        <label>Female</label>
+        <input type="radio" value="female" name="gender" v-model="form.gender"/>
         <br/><br/>
         <button v-on:click="login" type="button">Login</button>
     </form>
@@ -21,7 +46,10 @@ export default {
         return {
             form:{
                 email:'',
-                password:''
+                password:'',
+                country:'',
+                technology:[],
+                gender:'',
             }
         }
     },
