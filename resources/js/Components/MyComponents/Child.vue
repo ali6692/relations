b9e5fe96e9704ea36d95518e34e821fc4eef9395
Faraz <farazmsi2014@gmail.com>
2023-07-14 16:57:51 +0530

    multiple slot with examples

--- resources/js/Components/MyComponents/Child.vue
+++ resources/js/Components/MyComponents/Child.vue
@@ -1,8 +1,15 @@
 <template>
-    <slot></slot>
-
-
-
+    <div class="cart">
+    <header>
+        <slot name="header"></slot>
+    </header>
+    <content>
+        <slot name="main"></slot>
+    </content>
+    <footer>
+        <slot name="footer"></slot>
+    </footer>
+    </div>
 </template>
 
 <script>
@@ -12,5 +19,12 @@ export default {
 </script>
 
 <style scoped>
+.cart{
+    border:1px;
+    width:150px;
+    text-align:center;
+    padding:20px;
+}
+
 
 </style>
--- resources/js/Components/MyComponents/HelloWorld.vue
+++ resources/js/Components/MyComponents/HelloWorld.vue
@@ -1,27 +1,28 @@
 <template>
-    <h1>Slots in Vue js 3</h1>
-    <Child><h1>Code step by step</h1></Child>
-    <Child><a href="#">Code step by step</a></Child>
-    <div class="row">
-        <img width="100px" v-for="img in images" v-bind:src="img"/>
-    </div>
-    <Child><img src="/project-assets/images/20230603_131444.jpg"/></Child>
+    <Child>
+        <template v-slot:header>
+            <h1>Faraz</h1>
+        </template>
+        <template v-slot:main>
+            <p>A vue js product</p>
+        </template>
+        <button>Buy now</button>
+        <template v-slot:footer>
+            <button>Buy now</button>
+        </template>
+    </Child>
 </template>
+
 
 <script>
 import Child from "@/Components/MyComponents/Child.vue";
 export default {
     name: "HelloWorld",
     components: {
-        Child,
-        data()
-        {
-            return {
-                images:["/project-assets/images/20230603_131444.jpg"]
-            }
-        }
+        Child
     }
 }
+
 </script>
 <style scoped>
 h1 {
