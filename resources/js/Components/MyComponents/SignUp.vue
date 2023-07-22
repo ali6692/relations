5339ea3e28c643b1e55449c072b73d8d829c129d
Faraz <farazmsi2014@gmail.com>
2023-07-22 14:46:32 +0530

    integrated user sign up

--- resources/js/Components/MyComponents/SignUp.vue
+++ resources/js/Components/MyComponents/SignUp.vue
@@ -1,23 +1,47 @@
 <template>
     <img class="logo" src="project-assets/1.png">
-    <h1>Sign Up</h1>
     <div class="register">
-        <input type="text" placeholder="Enter your name"/>
-        <input type="text" placeholder="Enter your email"/>
-        <input type="password" placeholder="Enter your password"/>
-        <button>Sign Up</button>
+        <input type="text" v-model="name" placeholder="Enter your name"/>
+        <input type="text" v-model="email" placeholder="Enter your email"/>
+        <input type="password" v-model="password" placeholder="Enter your password"/>
+        <button v-on:click="signUp">Sign Up</button>
     </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-    name: "SignUp"
+    name: "SignUp",
+    data()
+    {
+        return {
+            name:'',
+            email:'',
+            password:''
+        }
+    },
+    methods:{
+       async signUp()
+        {
+            console.warn("signUp",this.name,this.email,this.password)
+            let result=await axios.post("http://127.0.0.1:8000/sample",{
+                email:this.email,
+                name:this.name,
+                password:this.password
+            });
+            console.warn(result);
+            if(result.status===201)
+            {
+                alert("Sign Up done");
+            }
+        }
+    }
 }
 </script>
 
 <style scoped>
 .logo{
-    width:300px;
+    width:250px;
 }
 .register input{
     width:300px;
@@ -30,7 +54,7 @@ export default {
     border:1px solid deepskyblue;
 }
 .register button{
-    width:320px;
+    width:325px;
     height:40px;
     border:1px solid deepskyblue;
     color:#fff;
